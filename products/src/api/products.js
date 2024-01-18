bdbe510241565a97a4d8a2b5d2fa4dc7f91daa58
Muhammad Muaz <muazgill435@gmail.com>
2024-01-18 21:35:20 +0500

    Products service changed to microservice

--- products/src/api/products.js
+++ products/src/api/products.js
@@ -1,12 +1,9 @@
 const ProductService = require('../services/product-service');
-const CustomerService = require('../services/customer-service');
 const UserAuth = require('./middlewares/auth')
 
 module.exports = (app) => {
     
     const service = new ProductService();
-    const customerService = new CustomerService();
-
 
     app.post('/product/create', async(req,res,next) => {
         
